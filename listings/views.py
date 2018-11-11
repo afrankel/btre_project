0ca168d07432d13eb4823b93a8b829f5578a16b0
afrankel <afrankel@solutionstreet.com>
2018-11-11 14:59:48 -0500

    adding listings and part of listing

--- listings/views.py
+++ listings/views.py
@@ -4,14 +4,17 @@ from .models import Listing
 
 def index(request):
   listings = Listing.objects.all()
-  print(listings)
   context = {
     'listings': listings
   }
   return render(request, 'listings/listings.html', context)
 
-def listing(request):
-  return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+  listing = Listing.objects.get(pk=listing_id)
+  context = {
+    'listing': listing
+  }
+  return render(request, 'listings/listing.html', context)
 
 def search(request):
   return render(request, 'listings/search.html')
