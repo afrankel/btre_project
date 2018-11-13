adca963c28c367b7e8d7ea3bad391acf2ea65929
afrankel <afrankel@solutionstreet.com>
2018-11-13 15:45:33 -0500

    commit point

--- listings/views.py
+++ listings/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
 def index(request):
-  listings = Listing.objects.all()
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
   context = {
-    'listings': listings
+    'listings': paged_listings
   }
   return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-  listing = Listing.objects.get(pk=listing_id)
+  listing = get_object_or_404(Listing, pk=listing_id)
   context = {
     'listing': listing
   }
