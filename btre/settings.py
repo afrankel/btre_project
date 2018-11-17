ac89080bc296a4c4705bc3eab3f1ab7edf9b0030
afrankel <afrankel@solutionstreet.com>
2018-11-17 18:44:29 -0500

    trying to fix database config

--- btre/settings.py
+++ btre/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = { 'default': update(db_from_env) }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
