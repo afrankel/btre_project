a849aa648bb95ffffd9377ae6826afde102e6432
afrankel <afrankel@solutionstreet.com>
2018-11-16 20:53:19 -0500

    alerts and register

--- btre/settings.py
+++ btre/settings.py
@@ -134,3 +134,8 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
